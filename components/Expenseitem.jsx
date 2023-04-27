d143ca434a3cbb3a0148be036628b39dd4c3ff5d
Ashish Chaudhari <122347602+ashishchaudhari1857@users.noreply.github.com>
2023-04-27 13:35:30 +0530

    Add files via upload

---
+++ components/Expenseitem.jsx
@@ -0,0 +1,12 @@
+const Expenseitem = () => {
+  return (
+    <div>
+      <h1>Expense Items</h1>
+      Food Rs 10 <br/>
+        Petrol Rs 100<br/>
+         Movies Rs 200
+    </div>
+  );
+}
+
+export default Expenseitem
